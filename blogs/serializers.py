a06a82366d5f39da9fb210662468537d45159d45
Pande Mukesh <pandeymukesh13>
2026-06-28 17:48:13 +0545

    Nested serializer with blogs and comments

--- blogs/serializers.py
+++ blogs/serializers.py
@@ -8,6 +8,7 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class BlogSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
     class Meta:
         model = Blog
         fields = "__all__"
