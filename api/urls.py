33261c06ddf48063757d6ec5e797ba4b2be6283b
Pande Mukesh <pandeymukesh13>
2026-06-28 17:59:00 +0545

    Primary-key base Operation on Blogs and Comments

--- api/urls.py
+++ api/urls.py
@@ -20,4 +20,7 @@ urlpatterns = [
 
         path('blogs/', views.BlogsView.as_view()),
         path('comments/', views.CommentsView.as_view()),
+
+        path ('blogs/<int:pk>/', views.BlogDetailView.as_view()),
+        path ('comments/<int:pk>/', views.CommentDetailView.as_view()),
 ]
