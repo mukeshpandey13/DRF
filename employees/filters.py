1e0f0c18c394cf3ba4eaf43c1842725f49e053ed
Pande Mukesh <pandeymukesh13>
2026-06-29 21:05:10 +0545

    Filter employees by name

--- employees/filters.py
+++ employees/filters.py
@@ -8,6 +8,8 @@ class EmployeeFilter(django_filters.FilterSet):
         lookup_expr='iexact'
     )
 
+    emp_name = django_filters.CharFilter(field_name='emp_name', lookup_expr='icontains')
+
     class Meta:
         model = Employee
-        fields = ['designation']
+        fields = ['designation','emp_name']
