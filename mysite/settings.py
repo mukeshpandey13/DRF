3c0341d18b29ea366bdf3e8f2c923d77f9081ba8
Pande Mukesh <pandeymukesh13>
2026-06-29 18:11:24 +0545

    Global LimitOffsetPagination

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,6 +123,10 @@ STATIC_URL = 'static/'
 
 ## Pagination ##
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE':2,
 }
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE':2,
+# }
