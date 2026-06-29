c2ff419ba76af9aeb4f7f52be596ef01dccd8fe7
Pande Mukesh <pandeymukesh13>
2026-06-29 21:12:15 +0545

    Filter employees by Id range

--- employees/filters.py
+++ employees/filters.py
@@ -9,7 +9,8 @@ class EmployeeFilter(django_filters.FilterSet):
     )
 
     emp_name = django_filters.CharFilter(field_name='emp_name', lookup_expr='icontains')
+    id = django_filters.RangeFilter(field_name='id')
 
     class Meta:
         model = Employee
-        fields = ['designation','emp_name']
+        fields = ['designation','emp_name','id']
